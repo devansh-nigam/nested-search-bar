bbce62c5e577e6ab0b417d9579a45661613235c0
devansh-nigam <devanshnigam0003@gmail.com>
2025-07-15 18:40:37 +0530

    Nested layout displaying level 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
+import searchData from "./mocks/searchData";
 
 function App() {
   const [searchText, setSearchText] = useState("");
+  const [stateSearchData, setStateSearchData] = useState();
+
+  useEffect(() => {
+    setStateSearchData(searchData);
+  }, []);
 
   const handleSearch = (event) => {
     setSearchText(event.target.value);
+  };
+
+  const SearchSection = () => {
+    return (
+      <div>
+        <div>
+          {stateSearchData?.length === 0 ? (
+            <div>No data found</div>
+          ) : (
+            stateSearchData?.map((item, index) => {
+              return <div key={index}>{item.value}</div>;
+            })
+          )}
+        </div>
+        <button>+ New Resource</button>
+      </div>
+    );
   };
 
   return (
@@ -17,6 +40,7 @@ function App() {
         onChange={handleSearch}
         placeholder="Enter value or search resources..."
       />
+      <SearchSection />
     </div>
   );
 }
