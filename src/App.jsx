a90fce69db38ea1451b762ceda49fabbd67a7443
devansh-nigam <devanshnigam0003@gmail.com>
2025-07-15 18:54:36 +0530

    Able to display right breadcrumbs

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import searchData from "./mocks/searchData";
 function App() {
   const [searchText, setSearchText] = useState("");
   const [stateSearchData, setStateSearchData] = useState();
+  const [breadcrumbs, setBreadcrumbs] = useState(["All resources"]);
 
   useEffect(() => {
     setStateSearchData(searchData);
@@ -17,12 +18,36 @@ function App() {
   const handleSearchItem = (item) => {
     if (item.type === "resource") {
       setStateSearchData(item.children);
+      const bcState = [...breadcrumbs, item.value];
+      setBreadcrumbs(bcState);
     }
+  };
+
+  const handleBreadcrumb = (index) => {
+    let data;
+    if (index === 0) {
+      data = searchData;
+      setSearchText("");
+    } else {
+      data = stateSearchData;
+    }
+    setBreadcrumbs(breadcrumbs.slice(0, index + 1));
+    setStateSearchData(data);
   };
 
   const SearchSection = () => {
     return (
-      <div>
+      <>
+        <div className="breadcrumbs">
+          {breadcrumbs?.map((breadcrumb, index) => {
+            return (
+              <div key={index} className="breadcrumb-item">
+                <div onClick={() => handleBreadcrumb(index)}>{breadcrumb}</div>
+                <span>{index !== breadcrumbs.length - 1 ? ">" : ""}</span>
+              </div>
+            );
+          })}
+        </div>
         <div>
           {stateSearchData?.length === 0 ? (
             <div>No data found</div>
@@ -42,7 +67,7 @@ function App() {
           )}
         </div>
         <button>+ New Resource</button>
-      </div>
+      </>
     );
   };
 
